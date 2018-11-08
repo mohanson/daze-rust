4943f1d5e5f634447a48e4e58ccfbad04c53e0e2
mohanson <mohanson@outlook.com>
2018-11-08 21:17:16 +0800

    2018-11-08 21:17:16

--- src/main.rs
+++ src/main.rs
@@ -4,51 +4,44 @@ extern crate md5;
 use crypto::rc4::Rc4;
 use std::error::Error;
 use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream};
+use std::net::Shutdown;
+use std::net::TcpListener;
+use std::net::TcpStream;
 use std::thread;
-use std::time;
 
 use crypto::symmetriccipher::SynchronousStreamCipher;
 
-static c_listen: &str = "0.0.0.0:51958";
-static c_cipher: &str = "daze";
+static C_LISTEN: &str = "0.0.0.0:51958";
+static C_CIPHER: &str = "daze";
 
-fn read_exact(
-    mut stream: &mut TcpStream,
-    c: &mut Rc4,
-    mut buf: &mut [u8],
-) -> Result<(), Box<Error>> {
-    let mut src = vec![0; buf.len()];
-    stream.read_exact(&mut src)?;
-    c.process(&src, buf);
-    Ok(())
-}
+// fn read_exact(stream: &mut TcpStream, c: &mut Rc4, buf: &mut [u8]) -> Result<(), Box<Error>> {
+//     let mut src = vec![0; buf.len()];
+//     stream.read_exact(&mut src)?;
+//     c.process(&src, buf);
+//     Ok(())
+// }
 
-fn read(mut stream: &mut TcpStream, c: &mut Rc4, mut buf: &mut [u8]) -> Result<usize, Box<Error>> {
+fn read(stream: &mut TcpStream, c: &mut Rc4, buf: &mut [u8]) -> Result<usize, Box<Error>> {
     let mut src = vec![0; buf.len()];
     let n = stream.read(&mut src)?;
-    c.process(&src, buf);
+    c.process(&src[..n], &mut buf[..n]);
     Ok(n)
 }
 
-fn daze(mut stream: TcpStream) {
+fn daze(mut src_stream: TcpStream) {
     let mut buf: Vec<u8> = vec![0; 128];
-    stream.read_exact(&mut buf).unwrap();
-    let mut raw: Vec<u8> = md5::compute(c_cipher).0.iter().cloned().collect();
+    src_stream.read(&mut buf).unwrap();
+    let mut raw: Vec<u8> = md5::compute(C_CIPHER).0.iter().cloned().collect();
     buf.append(&mut raw);
-    let mut c = Rc4::new(&buf);
-    let mut z = Rc4::new(&buf);
+    let mut cipher_a = Rc4::new(&buf);
+    let mut cipher_b = Rc4::new(&buf);
     let mut buf: Vec<u8> = vec![0; 12];
-    if let Err(err) = read_exact(&mut stream, &mut c, &mut buf) {
+    if let Err(err) = read(&mut src_stream, &mut cipher_a, &mut buf) {
         println!("{}", err);
         return;
     };
-    if buf[0] != 0xFF || buf[1] != 0xFF {
-        println!("daze: malformed request: {:?}", &buf[..2]);
-        return;
-    }
     let mut buf: Vec<u8> = vec![0; buf[11] as usize];
-    if let Err(err) = read_exact(&mut stream, &mut c, &mut buf) {
+    if let Err(err) = read(&mut src_stream, &mut cipher_a, &mut buf) {
         println!("{}", err);
         return;
     };
@@ -56,41 +49,46 @@ fn daze(mut stream: TcpStream) {
     println!("Connect {}", dst);
 
     let mut dst_stream = TcpStream::connect(&dst).unwrap();
-    let mut t_stream = stream.try_clone().unwrap();
-    let mut t_dst_stream = dst_stream.try_clone().unwrap();
-
-    thread::spawn(move || loop {
-        let mut buf: Vec<u8> = vec![0; 32*1024];
-        match read(&mut t_stream, &mut c, &mut buf) {
-            Ok(data) => {
-                if let Err(err) = &t_dst_stream.write(&buf[..data]) {
-                    break;
-                }
-                continue;
-            }
-            Err(err) => break,
+    let mut src_stream_copy = src_stream.try_clone().unwrap();
+    let mut dst_stream_copy = dst_stream.try_clone().unwrap();
+    thread::spawn(move || {
+        let mut buf: Vec<u8> = vec![0; 32 * 1024];
+        loop {
+            let n = match read(&mut src_stream_copy, &mut cipher_a, &mut buf) {
+                Ok(data) => data,
+                _ => 0,
+            };
+            if n == 0 {
+                break;
+            };
+            if dst_stream_copy.write_all(&buf[..n]).is_err() {
+                break;
+            };
         }
-        thread::sleep(time::Duration::from_secs(1));
+        src_stream_copy.shutdown(Shutdown::Both).unwrap();
+        dst_stream_copy.shutdown(Shutdown::Both).unwrap();
     });
 
-    let mut buf: Vec<u8> = vec![0; 32*1024];
+    let mut buf: Vec<u8> = vec![0; 32 * 1024];
     loop {
-        match read(&mut dst_stream, &mut z, &mut buf) {
-            Ok(data) => {
-                if let Err(err) = stream.write(&buf[..data]) {
-                    break;
-                }
-                continue;
-            }
-            Err(err) => break,
-        }
-        thread::sleep(time::Duration::from_secs(1));
+        let n = match read(&mut dst_stream, &mut cipher_b, &mut buf) {
+            Ok(data) => data,
+            _ => 0,
+        };
+        if n == 0 {
+            break;
+        };
+        if src_stream.write_all(&buf[..n]).is_err() {
+            break;
+        };
     }
+    dst_stream.shutdown(Shutdown::Both).unwrap();
+    src_stream.shutdown(Shutdown::Both).unwrap();
 }
 
 fn main() {
-    let listener = TcpListener::bind(c_listen).unwrap();
-    println!("Listen and server on {}", c_listen);
+    let listener = TcpListener::bind(C_LISTEN).unwrap();
+    println!("Listen and server on {}", C_CIPHER);
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
         thread::spawn(move || daze(stream));
