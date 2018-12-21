dbbfb9dc9b09f1184dcd5304378b6b930f612d6c
mohanson <mohanson@outlook.com>
2018-12-21 15:28:18 +0800

    Flow clippy

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,16 @@ use std::net;
 use std::thread;
 use std::time;
 
-fn daze(src_stream: net::TcpStream, k: &[u8]) -> Result<(), Box<error::Error>> {
+fn daze(src_stream: &net::TcpStream, k: &[u8]) -> Result<(), Box<error::Error>> {
     let mut src_reader = src_stream.try_clone()?;
     let mut src_writer = src_stream.try_clone()?;
     let mut key: Vec<u8> = vec![0; 128];
-    src_reader.read(&mut key)?;
+    src_reader.read_exact(&mut key)?;
     key.append(&mut Vec::from(k));
-    let mut src_reader = rc4::Reader::new(src_reader, key.as_slice())?;
+    let mut src_reader = rc4::Reader::init(src_reader, key.as_slice())?;
 
     let mut buf: Vec<u8> = vec![0; 12];
-    src_reader.read(&mut buf)?;
+    src_reader.read_exact(&mut buf)?;
     if buf[0] != 0xFF || buf[1] != 0xFF {
         return Err(From::from(format!(
             "daze: malformed request: {:?}",
@@ -36,13 +36,13 @@ fn daze(src_stream: net::TcpStream, k: &[u8]) -> Result<(), Box<error::Error>> {
         return Err(From::from(format!("daze: time span is too large: {}", sub)));
     };
     let mut buf: Vec<u8> = vec![0; buf[11] as usize];
-    src_reader.read(&mut buf)?;
+    src_reader.read_exact(&mut buf)?;
     let dst = String::from_utf8(buf).unwrap();
     println!("Connect {}", dst);
 
     let dst_stream = net::TcpStream::connect(&dst)?;
     let dst_reader = dst_stream.try_clone()?;
-    let mut dst_reader = rc4::Reader::new(dst_reader, key.as_slice())?;
+    let mut dst_reader = rc4::Reader::init(dst_reader, key.as_slice())?;
     let mut dst_writer = dst_stream.try_clone()?;
 
     thread::spawn(move || {
@@ -52,7 +52,7 @@ fn daze(src_stream: net::TcpStream, k: &[u8]) -> Result<(), Box<error::Error>> {
     Ok(())
 }
 
-fn hand(src_stream: net::TcpStream, k: &[u8]) {
+fn hand(src_stream: &net::TcpStream, k: &[u8]) {
     if let Err(err) = daze(src_stream, k) {
         println!("{:?}", err);
     }
@@ -72,12 +72,12 @@ fn main() {
         ap.parse_args_or_exit();
     }
 
-    let cipher: Vec<u8> = md5::compute(c_cipher).0.iter().cloned().collect();
+    let cipher: Vec<u8> = md5::compute(c_cipher).0.to_vec();
     println!("Listen and server on {}", c_listen);
     let listener = net::TcpListener::bind(&c_listen[..]).unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         let cipher = cipher.clone();
-        thread::spawn(move || hand(stream, cipher.as_slice()));
+        thread::spawn(move || hand(&stream, cipher.as_slice()));
     }
 }
