fe881ff6217725d17ef968f1edcff2aaad6ceea0
mohanson <mohanson@outlook.com>
2023-05-06 16:37:09 +0800

    Updates to adapts to the latest version of daze

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
+# Daze-Rust
+
 A Rust port of [daze](https://github.com/mohanson/daze).
-
-Tested in the following environments:
-
-- rustc 1.34.0
--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,78 @@
-use byteorder::ByteOrder;
-use std::error;
-use std::io;
-use std::io::prelude::*;
-use std::net;
-use std::thread;
-use std::time;
+use std::io::{Read, Write};
 
-fn daze(src_stream: &net::TcpStream, k: &[u8]) -> Result<(), Box<error::Error>> {
+fn salt(s: &str) -> Vec<u8> {
+    let mut cipher: Vec<u8> = md5::compute(s).to_vec();
+    cipher.extend(cipher.clone());
+    cipher.extend(cipher.clone());
+    cipher.extend(cipher.clone());
+    cipher
+}
+
+fn daze(src_stream: &std::net::TcpStream, k: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
     let mut src_reader = src_stream.try_clone()?;
-    let mut src_writer = src_stream.try_clone()?;
-    let mut key: Vec<u8> = vec![0; 128];
-    src_reader.read_exact(&mut key)?;
-    key.append(&mut Vec::from(k));
+    let src_writer = src_stream.try_clone()?;
+    let mut key: Vec<u8> = vec![0; 256];
+    src_reader.read_exact(&mut key[0..128])?;
+    key[128..256].copy_from_slice(k);
     let mut src_reader = rc4::Reader::new(src_reader, key.as_slice())?;
+    let mut src_writer = rc4::Writer::new(src_writer, key.as_slice())?;
 
-    let mut buf: Vec<u8> = vec![0; 12];
+    let mut buf: Vec<u8> = vec![0; 10];
     src_reader.read_exact(&mut buf)?;
-    if buf[0] != 0xFF || buf[1] != 0xFF {
-        return Err(From::from(format!(
-            "daze: malformed request: {:?}",
-            &buf[..2]
-        )));
-    };
-    let pit = byteorder::BigEndian::read_u64(&buf[2..10]);
-    let now = time::SystemTime::now()
-        .duration_since(time::UNIX_EPOCH)?
+    let pit = u64::from_be_bytes(buf[0..8].try_into().unwrap());
+    let now = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)?
         .as_secs();
     let sub = if now > pit { now - pit } else { pit - now };
     if sub > 120 {
-        return Err(From::from(format!("daze: time span is too large: {}", sub)));
+        return Err("daze: request expired".into());
     };
-    let mut buf: Vec<u8> = vec![0; buf[11] as usize];
+    let mut buf: Vec<u8> = vec![0; buf[9] as usize];
     src_reader.read_exact(&mut buf)?;
     let dst = String::from_utf8(buf).unwrap();
-    println!("Connect {}", dst);
+    println!("conn:   dial network=tcp address={}", dst);
 
-    let dst_stream = net::TcpStream::connect(&dst)?;
-    let dst_reader = dst_stream.try_clone()?;
-    let mut dst_reader = rc4::Reader::new(dst_reader, key.as_slice())?;
+    let dst_stream = std::net::TcpStream::connect(&dst)?;
+    let mut dst_reader = dst_stream.try_clone()?;
     let mut dst_writer = dst_stream.try_clone()?;
-
-    thread::spawn(move || {
-        io::copy(&mut src_reader, &mut dst_writer).ok();
+    src_writer.write_all(&[0])?;
+    std::thread::spawn(move || {
+        std::io::copy(&mut src_reader, &mut dst_writer).ok();
+        dst_stream.shutdown(std::net::Shutdown::Both).unwrap();
     });
-    io::copy(&mut dst_reader, &mut src_writer).ok();
+    std::io::copy(&mut dst_reader, &mut src_writer).ok();
+    src_stream.shutdown(std::net::Shutdown::Both).unwrap();
     Ok(())
 }
 
-fn hand(src_stream: &net::TcpStream, k: &[u8]) {
+fn hand(src_stream: &std::net::TcpStream, k: &[u8]) {
     if let Err(err) = daze(src_stream, k) {
-        println!("{:?}", err);
+        println!("conn:  error {}", err);
     }
+    println!("conn: closed")
 }
 
 fn main() {
-    let mut c_listen = String::from("127.0.0.1:51958");
+    let mut c_listen = String::new();
     let mut c_cipher = String::from("daze");
 
     {
         let mut ap = argparse::ArgumentParser::new();
-        ap.set_description("Start daze server");
         ap.refer(&mut c_listen)
-            .add_option(&["-l"], argparse::Store, "listen address");
+            .add_option(&["-l"], argparse::Store, "Listen address");
         ap.refer(&mut c_cipher)
-            .add_option(&["-k"], argparse::Store, "cipher, for encryption");
+            .add_option(&["-k"], argparse::Store, "Password");
         ap.parse_args_or_exit();
     }
 
-    let cipher: Vec<u8> = md5::compute(c_cipher).0.to_vec();
-    println!("Listen and server on {}", c_listen);
-    let listener = net::TcpListener::bind(&c_listen[..]).unwrap();
+    println!("main: server cipher is {}", c_cipher);
+    let cipher = salt(&c_cipher);
+    println!("main: listen and server on {}", c_listen);
+    let listener = std::net::TcpListener::bind(&c_listen[..]).unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
+        println!("conn: accept remote={}", stream.peer_addr().unwrap());
         let cipher = cipher.clone();
-        thread::spawn(move || hand(&stream, cipher.as_slice()));
+        std::thread::spawn(move || hand(&stream, cipher.as_slice()));
     }
 }
